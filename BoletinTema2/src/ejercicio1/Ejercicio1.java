8f0e6b1066d4b774bc9f3838889409465be25254
anavarro <anavarro@106-18.info2>
2022-10-04 11:47:36 +0200

    commit

--- BoletinTema2/src/ejercicio1/Ejercicio1.java
+++ BoletinTema2/src/ejercicio1/Ejercicio1.java
@@ -1,10 +1,26 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		int num1;
+		
+		System.out.println("Introduzca un numero");
+		num1 = sc.nextInt();
+		
+		num1 %=2;
+		
+		if (num1==0) {
+			System.out.println("El numero introducido es par");
+		}else {
+			System.out.println("El numero introducido es impar");
+		}
+		
+		sc.close();
 	}
 
 }
